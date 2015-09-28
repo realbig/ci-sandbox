65d03a358a583a5cc53414f1f3eac742d9db5ec0
joelworsham <joelworsham@gmail.com>
2015-09-28 13:43:34 -0400

    Initial commit

---
+++ plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: CI Testing Suite
+ * Description: A sandbox for testing unit testing... that sounds weird.
+ * Version: 0.1.0
+ * Author: Joel Worsham
+ */
+
+// I'm a plugin! hurmph-derrrr!
