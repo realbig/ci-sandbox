a9d3fcaa395f953e5496d434a8d610ffd0ae2fbf
joelworsham <joelworsham@gmail.com>
2015-10-02 08:41:13 -0400

    Move plugin.php to root

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Plugin Name: CI Testing Suite
- * Description: A sandbox for testing unit testing... that sounds weird.
- * Version: 0.1.0
- * Author: Joel Worsham
- */
-
-require_once __DIR__ . '/src/plugin.php';
